40de6f90876e7582b1bf1dc5234ba2323f3ef94d
rriswannn <142221552+rriswannn@users.noreply.github.com>
2023-10-26 11:26:33 +0700

    Push modul 2 ni bray

---
+++ MODUL2 RIZKY/modul2.php
@@ -0,0 +1,98 @@
+<?php
+
+$error = "";
+$status ="";
+if (isset($_SERVER["REQUEST_METHOD"]) =="POST") {
+
+    $tinggi = $_POST["tinggi_badan"];
+    $berat = $_POST["berat_badan"];
+    
+    
+
+    if ($tinggi == "" || $berat == ""){
+        $error = "tinggi_badan atau berat_badan tidak boleh kosong nilainya";
+    }
+
+    
+    else {
+        $tinggi_meter = $tinggi / 100;
+        $hasil = $berat/($tinggi_meter**2);
+
+        if($hasil <= 18.4){
+            $status = "Underweight";
+        }
+        elseif ($hasil <= 24.9){
+            $status = "Normal";
+        }
+        elseif ($hasil <= 39.9){
+            $status = "Overweight";
+        }
+        elseif ($hasil >= 40 ){
+            $status = "Obese";
+        }
+
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulator BMI</title>
+    <link rel="stylesheet" href="css.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-4">Kalkulator BMI</h4>
+                    <form action="" method="POST">
+                        <div class="form-floating mb-3">
+                            <input type="number" class="form-control" name="tinggi_badan" id="tinggi_badan" placeholder="175">
+                            <label for="tinggi_badan">Tinggi Badan (CM)</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="number" class="form-control" name="berat_badan" id="berat_badan" placeholder="53">
+                            <label for="berat_badan">Berat Badan (KG)</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-3 mt-3 w-100">Hitung BMI</button>
+                    </form>
+
+                    <!--  **********************  4  **************************     -->
+                    <!-- Hasilnya perhitungan BMI taruh di sini yaaa!! 😊 -->
+                    <!-- silakan taruh code kalian di bawah -->
+                    <p><?="Nilai BMI anda adalah ".$hasil ?></p>
+                    <p><?="Status anda adalah ".$status ?></p>
+
+            
+
+
+
+
+                    <!--  **********************  5  **************************     -->
+                    <!-- Hasil pesan error nya taruh di sini yaaa!! 😊  -->
+                    <!-- silakan taruh code kalian di bawah -->
+                    
+
+                    <!--  **********************  5  **************************     -->
+                  <p><?=$error?></p>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
